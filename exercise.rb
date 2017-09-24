2943768e2302c7c99e077d3d2d8f0cf00e5cba3a
Sam Furr <samneillfurr@gmail.com>
2017-09-24 11:48:30 -0700

    Add Description of Marklar code
    Add comments to describe how the pair of gsub methods do work on the given string.

--- exercise.rb
+++ exercise.rb
@@ -4,6 +4,8 @@ class Exercise
   # Return a string in which every word in "str" that exceeds 4 characters is replaced with "marklar".
   # If the word being replaced has a capital first letter, it should instead be replaced with "Marklar".
   def self.marklar(str)
+    #regex for any word over 4 letters and starts with a capital letter and replace it with "Marklar"
+    #run it again on the returned string looking for words of length greater than 4, making sure to ignore the new "Marklar"s and replacing with "marklar"
     str.gsub(/[A-Z]\w{5,}/, 'Marklar').gsub(/(?![Marklar])\w{5,}/, 'marklar')
   end
 
